7b95b9378c8b787e0b3a7ed8b261fa1a072d1fc0
exceed96 <exceed96@naver.com>
2024-10-24 12:51:39 +0900

    [Feat] 프로젝트 성과 수정

--- src/Data/Project/ProjectData.ts
+++ src/Data/Project/ProjectData.ts
@@ -6,54 +6,6 @@ import KtWizMain from "img/Projects/KtWiz/ktWizMain.jpg";
 
 const PROJECT_DATA = [
   {
-    name: "InsteadMemo",
-    mainImage: InsteadMain,
-    key: "insteadMemo",
-    type: "팀 프로젝트",
-    composition: "FrontEnd 1, BackEnd 2",
-    part: "FrontEnd",
-    period: "2023.01 ~ 2023.06",
-    detail:
-      "통합적인 메모 관리가 아닌 특정 URL을 추적하여서 메모를 저장하는 웹 확장 웹서비스입니다. 기존의 메모 확장 프로그램은 URL마다 구분 없이 통합적으로 메모를 저장하다 보니 저장한 메모가 많아진다면 특정 페이지에서 기억에 남기기 위해 저장한 메모를 찾는데 불편함을 겪은 경험을 토대로 직접 개발하여 사용하고자 한 프로젝트입니다.",
-    stack: ["React", "CssModules", "JavaScript", "Redux", "Axios"],
-    tool: ["Github", "Figma", "AWS"],
-    majorFunction: [
-      "Google, Naver api를 이용한 로그인 구현",
-      "Whale manifests를 통한 URL추적",
-      "URL마다 사용자가 저장하고 싶은 메모 저장",
-    ],
-    myFunction: [
-      "Google, Naver api를 이용한 로그인 구현",
-      "로그인한 유저의 정보를 브라우저 Secure Cookie에 JWT를 통하여 관리",
-      "URL이 변경될때마다 해당 URL에 저장된 메모 데이터 호출",
-      "REST API를 이용한 메모 저장/편집, 중요메모 지정, 폴더를 생성하여 메모 저장",
-    ],
-    result: [
-      {
-        title: "웹 개발의 연결고리: REST API를 이용한 데이터 처리 경험",
-        content:
-          "✅ 처음으로 REST API를 이용하여 BackEnd와 데이터를 주고받고 데이터를 JavaScript의 메서드를 통하여 가공해서 사용하는 등 웹 개발에서 FrontEnd와 BackEnd가 어떤 방식으로 통신을 해야 하고 어떤 흐름을 구성해야 하는지 경험을 쌓을 수 있었습니다.",
-      },
-      {
-        title:
-          "로그인 인증: JWT, Secure Cookie, Local Storage을 이용한 유저 로그인 관리",
-        content:
-          "✅ 이론적으로만 알고 있던 로그인 한 유저의 정보와 보안성을 관리하는 다양한 방법을 프로젝트에 적용함으로써 각각의 효율성을 알게 되었습니다. 여러 가지 방법 중 선택한 방법은 localStorage에 만료시간이 짧은 Access Token을 저장하고 Access Token의 재발급 용도로 사용할 Refresh Token은 HTTP Only 옵션과 sameSite 옵션을 정의한 Secure Cookie에 정의함으로써 토큰이 외부로 노출될 가능성을 줄였습니다.",
-      },
-      {
-        title: "서비스화 과정: AWS, Cloud Flare, Nginx를 활용한 배포 경험",
-        content:
-          "✅ 서비스화를 하기 위한 배포 작업을 해봄으로써 어떤 사전 작업과 배포를 위해 필요한 게 무엇인지에 대해서 알게 되는 발판이 되었습니다. 서비스의 신뢰성을 높이기 위하여 HTTPS 프로토콜을 적용하고 이를 위한 SSL 인증서를 발급받아 Nginx에 정의하여 AWS의 EC2를 통하여 배포하였습니다.",
-      },
-      {
-        title:
-          "사용자 중심의 프로젝트: 불편함을 해소하는 경험으로부터 얻은 교훈",
-        content:
-          "✅ 실제 불편함을 겪어 개발을 시작한 프로젝트이므로 사용자의 관점에서 프레임을 짜는 방식 혹은 어떤 기능을 만들어야 긍정적인 반응이 나올지 생각하게 된 프로젝트입니다. 해당 경험을 통하여 이후 프로젝트에서도 기능을 만들거나 프레임을 구성할 때 항상 사용자의 관점에서 먼저 바라보게 하는 계기가 되었습니다.",
-      },
-    ],
-  },
-  {
     name: "Tscenping",
     mainImage: TscenMain,
     key: "tscenping",
@@ -181,7 +133,7 @@ const PROJECT_DATA = [
         title:
           "데이터 캐싱으로 효율적인 데이터 호출: ReactQuery를 활용한 채팅방 리스트 및 랭킹 리스트 관리",
         content:
-          "✅ ReactQuery를 이용하여 채팅방 리스트, 랭킹 리스트 데이터들은 캐싱 화하여 서버로의 데이터 중복 호출을 줄이고 클라이언트와 서버와의 데이터 호출 부담을 줄이도록 하였습니다.",
+          "✅ React Query의 서버상태관리 및 캐싱 기능을 이용하여 API호출 상태를 관리하고 데이터 캐싱을 통해 서버 호출을 최소화 시켰습니다.기존 Tscenping프로젝트보다 상태관리코드를 400줄에서 300줄로 간소화 시키고 데이터 캐싱으로 서버의 부담을 줄였습니다.",
       },
       {
         title:
@@ -226,6 +178,17 @@ const PROJECT_DATA = [
         title: "커스텀 페이지네이션: 제한된 환경에서의 기능 구현",
         content:
           "✅ 기존 페이지네이션 패키지를 사용하지 않고 직접 커스텀 페이지네이션을 구현함으로써, 제한된 환경에서도 기능을 직접 개발하는 경험을 쌓았습니다.",
+      },
+      {
+        title: "PWA적용 : 웹 애플리케이션을 넘어선 모바일 접근성 향상",
+        content:
+          "✅ 웹 환경에만 대응하는게 아닌 앱 환경에도 대응하기 위해 PWA를 적용시켜 다양한 플랫폼에도 접근하기 편하고 문제없이 동작하는 환경을 구축하였습니다.",
+      },
+      {
+        title:
+          "API 성능 향상을 위한 최적화 전략 : 서버 설정을 통한 사용자 UX개선",
+        content:
+          "✅ API호출 응답시간을 획기적으로 줄였습니다. 기존 600msAPI응답 시간과 120ms걸리는 호출시간을 서버 최적화를 통해 6배 빠른 100ms, 20ms정도로 단축하여 사용자 경험을 향상 시켰습니다.",
       },
     ],
   },
@@ -286,6 +249,54 @@ const PROJECT_DATA = [
         title: "Node.js 성능 개선: PM2를 활용한 멀티 프로세스 구조 구현",
         content:
           "✅ Node.js는 싱글 스레드 특성상 높은 트래픽 상황에서 성능 저하가 발생할 수 있습니다. 이를 해결하기 위해 PM2 패키지를 활용하여 멀티 프로세스 구조를 구현했습니다. 이 방식으로 클라우드 자원을 효율적으로 사용하면서 동시에 들어오는 요청을 여러 프로세스에 분산시켜 처리함으로써 실시간 채팅 시스템의 전반적인 성능을 개선했습니다.",
+      },
+    ],
+  },
+  {
+    name: "InsteadMemo",
+    mainImage: InsteadMain,
+    key: "insteadMemo",
+    type: "팀 프로젝트",
+    composition: "FrontEnd 1, BackEnd 2",
+    part: "FrontEnd",
+    period: "2023.01 ~ 2023.06",
+    detail:
+      "통합적인 메모 관리가 아닌 특정 URL을 추적하여서 메모를 저장하는 웹 확장 웹서비스입니다. 기존의 메모 확장 프로그램은 URL마다 구분 없이 통합적으로 메모를 저장하다 보니 저장한 메모가 많아진다면 특정 페이지에서 기억에 남기기 위해 저장한 메모를 찾는데 불편함을 겪은 경험을 토대로 직접 개발하여 사용하고자 한 프로젝트입니다.",
+    stack: ["React", "CssModules", "JavaScript", "Redux", "Axios"],
+    tool: ["Github", "Figma", "AWS"],
+    majorFunction: [
+      "Google, Naver api를 이용한 로그인 구현",
+      "Whale manifests를 통한 URL추적",
+      "URL마다 사용자가 저장하고 싶은 메모 저장",
+    ],
+    myFunction: [
+      "Google, Naver api를 이용한 로그인 구현",
+      "로그인한 유저의 정보를 브라우저 Secure Cookie에 JWT를 통하여 관리",
+      "URL이 변경될때마다 해당 URL에 저장된 메모 데이터 호출",
+      "REST API를 이용한 메모 저장/편집, 중요메모 지정, 폴더를 생성하여 메모 저장",
+    ],
+    result: [
+      {
+        title: "웹 개발의 연결고리: REST API를 이용한 데이터 처리 경험",
+        content:
+          "✅ 처음으로 REST API를 이용하여 BackEnd와 데이터를 주고받고 데이터를 JavaScript의 메서드를 통하여 가공해서 사용하는 등 웹 개발에서 FrontEnd와 BackEnd가 어떤 방식으로 통신을 해야 하고 어떤 흐름을 구성해야 하는지 경험을 쌓을 수 있었습니다.",
+      },
+      {
+        title:
+          "로그인 인증: JWT, Secure Cookie, Local Storage을 이용한 유저 로그인 관리",
+        content:
+          "✅ 이론적으로만 알고 있던 로그인 한 유저의 정보와 보안성을 관리하는 다양한 방법을 프로젝트에 적용함으로써 각각의 효율성을 알게 되었습니다. 여러 가지 방법 중 선택한 방법은 localStorage에 만료시간이 짧은 Access Token을 저장하고 Access Token의 재발급 용도로 사용할 Refresh Token은 HTTP Only 옵션과 sameSite 옵션을 정의한 Secure Cookie에 정의함으로써 토큰이 외부로 노출될 가능성을 줄였습니다.",
+      },
+      {
+        title: "서비스화 과정: AWS, Cloud Flare, Nginx를 활용한 배포 경험",
+        content:
+          "✅ 서비스화를 하기 위한 배포 작업을 해봄으로써 어떤 사전 작업과 배포를 위해 필요한 게 무엇인지에 대해서 알게 되는 발판이 되었습니다. 서비스의 신뢰성을 높이기 위하여 HTTPS 프로토콜을 적용하고 이를 위한 SSL 인증서를 발급받아 Nginx에 정의하여 AWS의 EC2를 통하여 배포하였습니다.",
+      },
+      {
+        title:
+          "사용자 중심의 프로젝트: 불편함을 해소하는 경험으로부터 얻은 교훈",
+        content:
+          "✅ 실제 불편함을 겪어 개발을 시작한 프로젝트이므로 사용자의 관점에서 프레임을 짜는 방식 혹은 어떤 기능을 만들어야 긍정적인 반응이 나올지 생각하게 된 프로젝트입니다. 해당 경험을 통하여 이후 프로젝트에서도 기능을 만들거나 프레임을 구성할 때 항상 사용자의 관점에서 먼저 바라보게 하는 계기가 되었습니다.",
       },
     ],
   },
